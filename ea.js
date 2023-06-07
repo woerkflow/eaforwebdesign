6965602438c5638f2f1c8174c2f314e1e5b02971
Florian <woerkflow@web.de>
2023-06-07 21:59:32 +0200

    Correction

--- ea.js
+++ ea.js
@@ -28,7 +28,7 @@ const EA = {
         return pop
     },
 
-    getRandomInt(min, max) :number {
+    getRandomInt(min, max) {
         const mini = Math.ceil(min)
         const maxi = Math.floor(max)
         return Math.floor(Math.random() * (maxi - mini + 1) + mini)
